8b144cb182d03bf5df8b6b4708dde5ad2b8cddf9
Adrian <Adrianlines@live.co.uk>
2019-09-25 14:24:55 +0100

    updates

--- index.php
+++ index.php
@@ -116,16 +116,17 @@ projs.scrollIntoView();
 	</script>
 
 
-	<div class = "flex-container-reverse">
-		<div class = "flex-item">
+	 <div class = "flex-container-reverse">
+		<!--<div class = "flex-item">
 			<ul class="flex-container">
             <li class="flex-itemnew card">
 			<div><img class = "cardimage" src= "images/Calendar.jpg"> </img></div>	
-			<p><b>DTF</b></p>
+			<p><b>DTF</b><br>
              This project was created using the dota2 coordinator api, the api holds around 4 billion records each of which represents a match played. The idea of this project was to pull the data in and store it in a SQL database available for use later on. I plan at somepoint to implement some sort of ai to determine patterns within the data.
             	I really enjoyed working on this one as it allowed me to really get into a large amount of data without having to create the data myself. It already exists and there's a lot of it.
+				<br>
 				<a class =  "icon" href = "https://github.com/AdrianLines/DotaCoffee"> <i class="devicon-github-plain icon"></i></a>
-
+</p>
 			</li>
 			  <li class="flex-itemnew"></li>
 			  <li class="flex-itemnew">3</li>
@@ -133,16 +134,35 @@ projs.scrollIntoView();
 			  <li class="flex-itemnew">5</li>
 			  <li class="flex-itemnew">6</li>
 			</ul>
+</div> -->
+
+<div class="parent">
+<div class="div1"> <div><img class = "cardimage" src= "images/Calendar.jpg"> </img></div>	
+			<p><b>DTF</b><br>
+             This project was created using the dota2 coordinator api, the api holds around 4 billion records each of which represents a match played. The idea of this project was to pull the data in and store it in a SQL database available for use later on. I plan at somepoint to implement some sort of ai to determine patterns within the data.
+            	I really enjoyed working on this one as it allowed me to really get into a large amount of data without having to create the data myself. It already exists and there's a lot of it.
+				<br>
+				<a class =  "icon" href = "https://github.com/AdrianLines/DotaCoffee"> <i class="devicon-github-plain icon"></i></a>
+</p></div>
+<div class="div2"> </div>
+<div class="div3"> </div>
+<div class="div4"> </div>
+<div class="div5"> </div>
+<div class="div6"> </div>
+<div class="div7"> </div>
+<div class="div8"> </div>
+<div class="div9"> </div>
+<div class="div10"> </div>
+<div class="div11"> </div>
+<div class="div12"> </div>
 </div>
+
 
 <div class = "flex-item2">
 	<div class ="divheader">
 		<?php include "projects.svg"?>
 	</div>
-		<div class = "inner-div">
-		<!--	<img class = "image"src ="Images/coffee1.jpg"> -->
-
-	</div>
+		
 
 <?php>		$ch = curl_init();
 
